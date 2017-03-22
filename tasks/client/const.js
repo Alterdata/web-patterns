016d8c057fb4e2d33f7b1e67d1cde4a30d1bc95c
Bernardo Esbérard <bernardo.dsn.erp@alterdata.com.br>
2017-03-22 14:49:02 -0300

    Ajusta a constante de build.

--- tasks/client/const.js
+++ tasks/client/const.js
@@ -15,5 +15,5 @@ export const tasks = {
   CLIENT_COVERAGE: 'client.coverage',
   CLIENT_WATCH: 'client.watch',
   CLIENT_RELOAD: 'client.reload',
-  CLIENT_BUILD_DIST: 'npm run build-dist'
+  CLIENT_BUILD_DIST: 'client.build:dist'
 };
